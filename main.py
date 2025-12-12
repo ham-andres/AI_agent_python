40df66b562b17fac47a5220ce300fb2523dbd93e
Hamandres <asd.ray6@gmail.com>
2025-12-12 07:44:08 +0000

    added data usage metadata for token analysis

--- main.py
+++ main.py
@@ -4,19 +4,30 @@ from google import genai
 
 
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-if api_key == None:
-    raise "RuntimeError"
-
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
-print(response.text)
 def main():
-    print("Hello from ai-agent-python!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key == None:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+
+    client = genai.Client(api_key=api_key)
+
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents=prompt,
+    )
+    
+    if response.usage_metadata == None:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+    else:
+        print("User prompt:", prompt)
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Response:")
+        print(response.text)
+
 
 
 if __name__ == "__main__":
