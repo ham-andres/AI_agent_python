81dc3e57370f7fa9b60e61aa650b15b5461fd34b
Hamandres <asd.ray6@gmail.com>
2025-12-13 06:15:43 +0000

    argeparser added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 
 
@@ -14,15 +15,19 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    parser = argparse.ArgumentParser(description = "Your personal AI")
+    parser.add_argument("user_prompt",type = str, help = "Enter your prompt")
+    args = parser.parse_args()
+
+    # prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=prompt,
+        model='gemini-2.5-flash', contents=args.user_prompt,
     )
     
     if response.usage_metadata == None:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
     else:
-        print("User prompt:", prompt)
+        print("User prompt:", args.user_prompt)
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
         print("Response:")
